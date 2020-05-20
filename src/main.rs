95e89df0b92773794db36352a046cb4e70e92c50
Radek Krahl <radek@krahl.pl>
2020-05-20 08:58:37 +0000

    Factor out `run` function

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,58 @@ enum Ops {
     NOP,
 }
 
-fn main() -> io::Result<()> {
-    let mut buffer = Vec::new();
-    io::stdin().read_to_end(&mut buffer)?;
+fn run(ops: Vec<Ops>) {
+    let mut memory: [u8; MAX_MEM] = [0; MAX_MEM];
+    let mut pc: usize = 0;
+    let mut ptr: usize = 0;
+    let program_size = ops.len();
 
-    let intermediate_program: Vec<&u8> = buffer
+    while pc < program_size {
+        match ops[pc] {
+            Ops::INP => {
+                ptr += 1;
+                pc += 1;
+            }
+            Ops::DEP => {
+                ptr -= 1;
+                pc += 1;
+            }
+            Ops::INC => {
+                memory[ptr] = memory[ptr].wrapping_add(1);
+                pc += 1;
+            }
+            Ops::DEC => {
+                memory[ptr] = memory[ptr].wrapping_sub(1);
+                pc += 1;
+            }
+            Ops::PRN => {
+                print!("{}", memory[ptr] as char);
+                pc += 1;
+            }
+            Ops::RDC => {
+                /* FIXME: read char */
+                pc += 1;
+            }
+            Ops::BRZ(addr) => {
+                if memory[ptr] == 0 {
+                    pc = addr;
+                } else {
+                    pc += 1
+                }
+            }
+            Ops::RET(addr) => {
+                pc = addr;
+            }
+            Ops::NOP => (),
+        }
+    }
+}
+
+fn main() -> io::Result<()> {
+    let mut input = Vec::new();
+    io::stdin().read_to_end(&mut input)?;
+
+    let intermediate_program: Vec<&u8> = input
         .iter()
         .filter(|b| match **b {
             b'>' | b'<' | b'+' | b'-' | b'.' | b',' | b'[' | b']' => true,
@@ -49,7 +96,7 @@ fn main() -> io::Result<()> {
                     }
                 }
                 Ops::BRZ(pc + 1)
-            },
+            }
             b']' => {
                 let mut pc = cur_pc;
                 let mut loop_count = 1;
@@ -62,59 +109,13 @@ fn main() -> io::Result<()> {
                     }
                 }
                 Ops::RET(pc)
-            },
+            }
             _ => Ops::NOP,
         })
         .collect();
 
-    let program_size = program.len();
-    if program_size != 0 {
-        println!("Starting program in 3.. 2.. 1.. Now!");
-
-        let mut memory: [u8; MAX_MEM] = [0; MAX_MEM];
-        let mut pc: usize = 0;
-        let mut ptr: usize = 0;
-
-        while pc < program_size {
-            match program[pc] {
-                Ops::INP => {
-                    ptr = if ptr != MAX_MEM { ptr + 1 } else { 0 };
-                    pc += 1;
-                }
-                Ops::DEP => {
-                    ptr = if ptr != 0 { ptr - 1 } else { MAX_MEM };
-                    pc += 1;
-                }
-                Ops::INC => {
-                    memory[ptr] = memory[ptr].wrapping_add(1);
-                    pc += 1;
-                }
-                Ops::DEC => {
-                    memory[ptr] = memory[ptr].wrapping_sub(1);
-                    pc += 1;
-                }
-                Ops::PRN => {
-                    print!("{}", memory[ptr] as char);
-                    pc += 1;
-                }
-                Ops::RDC => {
-                    /* FIXME: read char */
-                    pc += 1;
-                }
-                Ops::BRZ(addr) => {
-                    if memory[ptr] == 0 {
-                        pc = addr;
-                    } else {
-                        pc += 1
-                    }
-                }
-                Ops::RET(addr) => {
-                    pc = addr;
-                }
-                Ops::NOP => (),
-            }
-        }
-    }
+    println!("Starting program in 3.. 2.. 1.. Now!");
+    run(program);
 
     Ok(())
 }
