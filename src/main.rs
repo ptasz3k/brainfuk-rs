8c748ec7991bb0959a1cd251bb1af12a79512939
Radek Krahl <radek@krahl.pl>
2020-05-20 10:04:51 +0000

    Loops as nested instruction lists

--- src/main.rs
+++ src/main.rs
@@ -2,120 +2,113 @@ use std::io::{self, Read};
 
 const MAX_MEM: usize = 64 * 1024;
 
-#[derive(PartialEq)]
-enum Ops {
+#[derive(Clone, Debug)]
+enum Tok {
     INP,
     DEP,
     INC,
     DEC,
     PRN,
     RDC,
-    BRZ(usize),
-    RET(usize),
-    NOP,
+    BRZ,
+    RET,
 }
 
-fn run(ops: Vec<Ops>) {
-    let mut memory: [u8; MAX_MEM] = [0; MAX_MEM];
-    let mut pc: usize = 0;
-    let mut ptr: usize = 0;
-    let program_size = ops.len();
+#[derive(Debug)]
+enum Op {
+    INP,
+    DEP,
+    INC,
+    DEC,
+    PRN,
+    RDC,
+    LOOP(Vec<Op>),
+}
 
-    while pc < program_size {
-        match ops[pc] {
-            Ops::INP => {
-                ptr += 1;
-                pc += 1;
-            }
-            Ops::DEP => {
-                ptr -= 1;
-                pc += 1;
-            }
-            Ops::INC => {
-                memory[ptr] = memory[ptr].wrapping_add(1);
-                pc += 1;
-            }
-            Ops::DEC => {
-                memory[ptr] = memory[ptr].wrapping_sub(1);
-                pc += 1;
-            }
-            Ops::PRN => {
-                print!("{}", memory[ptr] as char);
-                pc += 1;
-            }
-            Ops::RDC => {
-                /* FIXME: read char */
-                pc += 1;
-            }
-            Ops::BRZ(addr) => {
-                if memory[ptr] == 0 {
-                    pc = addr;
-                } else {
-                    pc += 1
+fn run(ops: &Vec<Op>, mem: &mut [u8], ptr: &mut usize) {
+    for op in ops {
+        match op {
+            Op::INP => *ptr += 1,
+            Op::DEP => *ptr -= 1,
+            Op::INC => mem[*ptr] += 1,
+            Op::DEC => mem[*ptr] -= 1,
+            Op::PRN => print!("{}", mem[*ptr] as char),
+            Op::RDC => { /* FIXME: read char */ }
+            Op::LOOP(inner) => {
+                while mem[*ptr] != 0 {
+                    run(&inner, mem, ptr)
                 }
             }
-            Ops::RET(addr) => {
-                pc = addr;
-            }
-            Ops::NOP => (),
         }
     }
 }
 
+fn tokenize(input: &str) -> Vec<Tok> {
+    input
+        .chars()
+        .filter_map(|b| match b {
+            '>' => Some(Tok::INP),
+            '<' => Some(Tok::DEP),
+            '+' => Some(Tok::INC),
+            '-' => Some(Tok::DEC),
+            '.' => Some(Tok::PRN),
+            ',' => Some(Tok::RDC),
+            '[' => Some(Tok::BRZ),
+            ']' => Some(Tok::RET),
+            _ => None,
+        })
+        .collect()
+}
+
+fn parse(tokens: &Vec<Tok>) -> Vec<Op> {
+    let mut program: Vec<Op> = Vec::new();
+
+    let mut loop_end = 0;
+    tokens.iter().enumerate().for_each(|(i, t)| {
+        if i >= loop_end {
+            match t {
+                Tok::INP => program.push(Op::INP),
+                Tok::DEP => program.push(Op::DEP),
+                Tok::INC => program.push(Op::INC),
+                Tok::DEC => program.push(Op::DEC),
+                Tok::PRN => program.push(Op::PRN),
+                Tok::RDC => program.push(Op::RDC),
+                Tok::BRZ => {
+                    let mut pc = i;
+                    let mut loop_count = 1;
+                    while loop_count != 0 {
+                        pc += 1;
+                        match tokens[pc] {
+                            Tok::BRZ => loop_count += 1,
+                            Tok::RET => loop_count -= 1,
+                            _ => (),
+                        }
+                    }
+                    loop_end = pc;
+                    program.push(Op::LOOP(parse(&tokens[i + 1..pc].to_vec())))
+                }
+                _ => (),
+            }
+        }
+    });
+
+    program
+}
+
 fn main() -> io::Result<()> {
-    let mut input = Vec::new();
-    io::stdin().read_to_end(&mut input)?;
+    let mut input: String = String::from("");
+    io::stdin().read_to_string(&mut input)?;
 
-    let intermediate_program: Vec<&u8> = input
-        .iter()
-        .filter(|b| match **b {
-            b'>' | b'<' | b'+' | b'-' | b'.' | b',' | b'[' | b']' => true,
-            _ => false,
-        })
-        .collect();
+    let tokens = tokenize(&input);
+    let program = parse(&tokens);
 
-    let program: Vec<Ops> = intermediate_program
-        .iter()
-        .enumerate()
-        .map(|(cur_pc, b)| match *b {
-            b'>' => Ops::INP,
-            b'<' => Ops::DEP,
-            b'+' => Ops::INC,
-            b'-' => Ops::DEC,
-            b'.' => Ops::PRN,
-            b',' => Ops::RDC,
-            b'[' => {
-                let mut pc = cur_pc;
-                let mut loop_count = 1;
-                while loop_count != 0 {
-                    pc += 1;
-                    if *intermediate_program[pc] == b'[' {
-                        loop_count += 1;
-                    } else if *intermediate_program[pc] == b']' {
-                        loop_count -= 1;
-                    }
-                }
-                Ops::BRZ(pc + 1)
-            }
-            b']' => {
-                let mut pc = cur_pc;
-                let mut loop_count = 1;
-                while loop_count != 0 {
-                    pc -= 1;
-                    if *intermediate_program[pc] == b']' {
-                        loop_count += 1;
-                    } else if *intermediate_program[pc] == b'[' {
-                        loop_count -= 1;
-                    }
-                }
-                Ops::RET(pc)
-            }
-            _ => Ops::NOP,
-        })
-        .collect();
+    // println!("{:?}", tokens);
+    // println!("{:?}", program);
 
     println!("Starting program in 3.. 2.. 1.. Now!");
-    run(program);
+    let mut memory: [u8; MAX_MEM] = [0; MAX_MEM];
+    let mut ptr: usize = 0;
+    run(&program, &mut memory, &mut ptr);
 
     Ok(())
 }
